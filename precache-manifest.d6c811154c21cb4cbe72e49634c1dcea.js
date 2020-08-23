8a771114d1e91b2565bdee8e41fde4f423790479
Andrew Wolcott <andrewjwolcott@gmail.com>
2020-08-23 14:07:37 -0700

    Updates

---
+++ precache-manifest.d6c811154c21cb4cbe72e49634c1dcea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9be9b1d4d37d3133d3977dfbc40bf18",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "32abdb6558ebae9c5d82",
+    "url": "/monsters-rolodex/static/css/main.7f024b9b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32abdb6558ebae9c5d82",
+    "url": "/monsters-rolodex/static/js/main.19a9482f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
